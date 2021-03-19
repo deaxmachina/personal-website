39bd8099bd5a0cf6ad488a3994dae71f56c5210a
deaxmachina <d.y.bankova@gmail.com>
2021-03-19 23:30:52 +0900

    shape adjustments

--- src/Draft/HeaderVizHtml.js
+++ src/Draft/HeaderVizHtml.js
@@ -5,8 +5,10 @@ import { gsap, ScrollTrigger, Draggable, MotionPathPlugin, CustomWiggle, TweenMa
 // don't forget to register plugins
 gsap.registerPlugin(ScrollTrigger, Draggable, MotionPathPlugin);
 
+const blueColour = "#0D7583"
 
-const opacityLow = 0.4;
+
+const opacityLow = 0.45;
 const opacityHigh = 0.4
 
 const HeaderViz = ({ windowWidth, windowHeight }) => {
@@ -88,12 +90,12 @@ const HeaderViz = ({ windowWidth, windowHeight }) => {
 
         <g transform={`scale(${0.0005*windowWidth})`}>  
           <path className="card-bg" fill="#0A1014" fill="url(#radial-gradient)" fill-opacity="0.8" d="M257.73 91.822L542.176 144.318L585.141 453.52L320.013 633.352L79.4997 521L22.4999 211.5L257.73 91.822Z" />
-          <path className="card-bg" fill="#4F3C76" filter="url(#glow)" fill-opacity={opacityHigh} d="M186.179 148.197L438.725 40.1062L677.481 284.274L591.002 527.287L339.274 579.77L78.0649 404.64L186.179 148.197Z" />
-          <path className="card-bg" filter="url(#static-noise)" fill="#2177A7" fill-opacity={opacityLow} d="M285.308 192.756L493 257.231L450.826 456.756L193 440.201L285.308 192.756Z"/>
-          <path className="card-bg" filter="url(#static-noise)" fill="#2177A7"  fill-opacity={opacityLow} d="M531.539 192.411L623.087 395.298L438.961 503.591L275.889 296.366L531.539 192.411Z"/>
-          <path className="card-bg" fill="#2177A7"  fill-opacity={opacityLow} d="M279.277 453.039L213.451 288.121L402.972 135.38L537.285 284.5L279.277 453.039Z"/>
-          <rect className="card-bg" fill="#2177A7"  fill-opacity={opacityLow} width="412.224" height="337.291" transform="matrix(0.388631 -0.921394 0.926701 0.375798 127.2 419.359)"/>
-          <path className="card-bg" fill="#2177A7"  fill-opacity={opacityLow} d="M291.143 109.384L533.855 345.363L391.488 500.329L197.034 535.918L52.7508 319.903L291.143 109.384Z"/>
+          <path className="card-bg" fill="#4C4976" filter="url(#glow)" fill-opacity={opacityHigh} d="M186.179 148.197L438.725 40.1062L677.481 284.274L591.002 527.287L339.274 579.77L78.0649 404.64L186.179 148.197Z" />
+          <path className="card-bg" filter="url(#static-noise)" fill={blueColour} fill-opacity={opacityLow} d="M285.308 192.756L493 257.231L450.826 456.756L193 440.201L285.308 192.756Z"/>
+          <path className="card-bg" filter="url(#static-noise)" fill={blueColour}  fill-opacity={opacityLow} d="M531.539 192.411L623.087 395.298L438.961 503.591L275.889 296.366L531.539 192.411Z"/>
+          <path className="card-bg" filter="url(#static-noise)" fill={blueColour}  fill-opacity={opacityLow} d="M279.277 453.039L213.451 288.121L402.972 135.38L537.285 284.5L279.277 453.039Z"/>
+          <rect className="card-bg" fill={blueColour} fill-opacity={opacityLow} width="412.224" height="337.291" transform="matrix(0.388631 -0.921394 0.926701 0.375798 127.2 419.359)"/>
+          <path className="card-bg" fill={blueColour}  fill-opacity={opacityLow} d="M291.143 109.384L533.855 345.363L391.488 500.329L197.034 535.918L52.7508 319.903L291.143 109.384Z"/>
           <rect className="test" fill="#EBEBEB" filter="url(#glow)" x="365.37" y="227.629" width="106" height="105.55" transform="rotate(43.4472 365.37 227.629)"/>
         </g>
         
