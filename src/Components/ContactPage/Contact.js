000d7ab6ce28e3c941ef15aa2041fa75ef903575
deaxmachina <d.y.bankova@gmail.com>
2021-03-27 19:32:13 +0900

    added decoration to contact page

--- src/Components/ContactPage/Contact.js
+++ src/Components/ContactPage/Contact.js
@@ -45,6 +45,31 @@ const ContactSocial = () => {
   )
 }
 
+const ContactDecoration = ({ id }) => {
+  const blueColour = "#0D8383" 
+  const purpleColour =  "#4E1B34" 
+  const lightColour = "#ebebeb"
+  const opacity = 0.5;
+
+  return (
+    <>
+    <svg width="447" height="394" id={id}>
+      <filter id="glow-contact">
+        <feGaussianBlur stdDeviation="5.0" result='colouredBlur'/>
+        <feMerge>
+          <feMergeNode in='colouredBlur' />
+          <feMergeNode in='SourceGraphic' />
+        </feMerge>
+      </filter>
+
+      <rect x="104" y="140" width="238" height="125" fill={lightColour} filter="url(#glow-contact)" fill-opacity={opacity} />
+      <rect x="172.547" y="51" width="242" height="242" transform="rotate(45.48 172.547 51)" fill={blueColour} fill-opacity={opacity}/>
+      <rect x="270.2" width="242" height="242" transform="rotate(43.3754 270.2 0)" fill={purpleColour} fill-opacity={opacity}/>
+    </svg>
+    </>
+  )
+};
+
 
 
 const Contact = () => {
@@ -53,6 +78,8 @@ const Contact = () => {
       <ContactTitle />
       <ContactBox />
       <ContactSocial />
+      <ContactDecoration id="contact__decoration-l"/>
+      <ContactDecoration id="contact__decoration-r"/>
       <h1>Text</h1>
     </section>
   )
