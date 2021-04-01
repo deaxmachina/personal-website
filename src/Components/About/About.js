7d314b547a7033f244df6cc85ebff3918af97b7b
deaxmachina <d.y.bankova@gmail.com>
2021-04-01 19:03:12 +0900

    about page restructiring

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -77,13 +77,15 @@ const About = ({ windowWidth, windowHeight }) => {
   return (
     <section id="about__page-container">
       <div id="about__about-container">
-        <AboutText />
+        
         <PhotoDecoration
             scaleFactor={ 
               windowWidth > 1300 ? 1.3: windowWidth > 1100 ? 1 : windowWidth > 900 ? 0.85 : windowWidth > 600 ? 0.90 : windowWidth > 400 ? 0.80 : 0.70
             }
             imgUrl="https://live.staticflickr.com/65535/51077543302_8c5ce50658_b.jpg"
         />
+        <AboutText />
+        
       </div>
       <CVTimeline windowWidth={windowWidth} windowHeight={windowHeight}/>
     </section>
