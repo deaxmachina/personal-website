7e473d26bd405d674ac13fb59d43ffc303dbb22c
deaxmachina <d.y.bankova@gmail.com>
2021-04-12 13:39:29 +0900

    using hash link to get a tag link to work

--- src/Components/HeroPage/HeroMenu.js
+++ src/Components/HeroPage/HeroMenu.js
@@ -1,16 +1,17 @@
 import React, { useEffect } from "react";
 import "./HeroPage.css";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
+import { HashLink as Link } from 'react-router-hash-link';
 
 
 const HeroMenu = () => {
   return (
     <>
       <div id="hero-menu-container">
-        <Link to={`portfolio`}><div className="menu-item">projects</div></Link>
-        <Link to={`/about`}><div className="menu-item">about</div></Link>
+        <Link to="portfolio"><div className="menu-item">projects</div></Link>
+        <Link to="/about"><div className="menu-item">about</div></Link>
         <div className="menu-item">writing</div>
-        <a href="#contact-page"><div className="menu-item">contact</div></a>
+        <Link to="#contact-page"><div className="menu-item">contact</div></Link>
       </div >
       
       <div className="menu-underline"></div>
